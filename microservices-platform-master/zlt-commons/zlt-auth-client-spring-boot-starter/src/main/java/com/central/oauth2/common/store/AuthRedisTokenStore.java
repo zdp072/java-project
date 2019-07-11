b679f7a8c400bff56c185420183d3e2012d7ba95
zdp072 <zdp072@163.com>
2019-07-12 00:45:47 +0800

    init

---
+++ microservices-platform-master/zlt-commons/zlt-auth-client-spring-boot-starter/src/main/java/com/central/oauth2/common/store/AuthRedisTokenStore.java
@@ -0,0 +1,23 @@
+package com.central.oauth2.common.store;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * 认证服务器使用Redis存取令牌
+ * 注意: 需要配置redis参数
+ *
+ * @author zlt
+ * @date 2018/7/25 9:36
+ */
+public class AuthRedisTokenStore {
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new CustomRedisTokenStore(connectionFactory);
+    }
+}
